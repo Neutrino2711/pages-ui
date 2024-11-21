86633924e6a56b753b58ff164e66830038478ecc
Neutrino2711 <akshajpal24@gmail.com>
2024-11-21 11:23:34 +0530

    further refactoring

--- lib/widgets/swipable_element.dart
+++ lib/widgets/swipable_element.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:swipe_start/widgets/bottom_section.dart';
+import 'package:swipe_start/widgets/middle_section.dart';
+import 'package:swipe_start/widgets/topText_section.dart';
 
-import '../decorations/custom_color.dart';
-import '../constants/constants.dart';
 
 class SwipableElement extends StatelessWidget {
-   const SwipableElement({
+  const SwipableElement({
     super.key,
     required this.topTextfirst,
     required this.topTextsecond,
@@ -19,76 +20,24 @@ class SwipableElement extends StatelessWidget {
   final String bottomText;
   final bool isFirst;
 
-  
-
   @override
   Widget build(BuildContext context) {
-
-    final TextConstants  textConstants = TextConstants();
-    final customColors = Theme.of(context).extension<CustomColors>()!;
-    final textTheme = Theme.of(context).textTheme;
-
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text(
-          topTextfirst,
-          style: textTheme.headlineSmall,
+        TopTextSection(
+          topTextfirst: topTextfirst,
+          topTextsecond: topTextsecond,
+          isFirst: isFirst,
         ),
-        isFirst
-            ? Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    textConstants.kPage1TopTextSecond,
-                    style: textTheme.titleLarge, 
-                  ),
-                  Text.rich(
-                    TextSpan(
-                      children: [
-                        TextSpan(
-                          text: textConstants.kPage1TopTextSecondFirstHalf, 
-                          style: textTheme.titleLarge!.copyWith(
-                            color: Colors.black, 
-                          ),
-                        ),
-                        TextSpan(
-                          text: textConstants.kPage1TopTextSecondSecondHalf, 
-                          style: textTheme.titleLarge!.copyWith(
-                            color: customColors.swipeBorderColor,
-                          ),
-                        ),
-                      ],
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              )
-            : Text(
-                topTextsecond,
-                style: textTheme.titleLarge!.copyWith(
-                  color: const Color(0xFF2FA270), 
-                ),
-              ),
-        const SizedBox(
-          height: 30,
+        SizedBox(height: 
+        MediaQuery.of(context).size.width * 0.1
         ),
-        Image.asset(
-          foreground,
-          height: MediaQuery.of(context).size.height * .4,
-        ),
-         SizedBox(
-          height: MediaQuery.of(context).size.height * 0.05,
-        ),
-        Text(
-          bottomText,
-          textAlign: TextAlign.center,
-          style: textTheme.bodySmall!.copyWith(
-            color: Colors.black,
-            fontWeight: FontWeight.w500,
-          ), 
-        ),
+        MiddleImageSection(foreground: foreground),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+        BottomTextSection(bottomText: bottomText),
       ],
     );
   }
 }
+
